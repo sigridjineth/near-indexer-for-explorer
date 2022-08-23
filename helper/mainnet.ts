a787ecf200ff7ddbfcb00f5554957426792035d2
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-08-23 19:24:29 +0900

    refactor: add entry parse logic for mainnet

--- helper/mainnet.ts
+++ helper/mainnet.ts
@@ -1,5 +1,6 @@
 import { startStream, types } from 'near-lake-framework';
 import { Logger } from "tslog";
+import {DeleteAccountAction} from "near-lake-framework/dist/types";
 const log: Logger = new Logger();
 
 const lakeConfigMainnet: types.LakeConfig = {
@@ -11,12 +12,12 @@ const lakeConfigMainnet: types.LakeConfig = {
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'test',
-    password: 'password',
+    user: process.env.MYSQL_DID,
+    password: process.env.MYSQL_PWD,
     database: 'near_data',
 });
 
-log.info(" MySQL Connection Established For Mainnet Inserting >>>>>>>>>>>>>>>>>>>>>>>>>> ");
+log.info(" MySQL Connection Established For Mainnet Inserting >>>>>>>>>>>>>>>>>>>>");
 connection.connect();
 
 // setting for mainnet
@@ -40,6 +41,11 @@ connection.query(
     "          id    INT PRIMARY KEY AUTO_INCREMENT,\n" +
     "          address_id VARCHAR(100) NOT NULL DEFAULT '',\n" +
     "          FK_PUBLIC_KEY_ID INT,\n" +
+    "          block_height BIGINT NOT NULL,\n" +
+    "          receipt_hash VARCHAR(100),\n" +
+    "          delete_receipt_hash VARCHAR(100),\n" +
+    "          save_type VARCHAR(100) NOT NULL,\n" +
+    "          is_deleted BOOLEAN NOT NULL DEFAULT 0," +
     "          FOREIGN KEY (FK_PUBLIC_KEY_ID)\n" +
     "          REFERENCES PUBLIC_KEY_MAINNET(id)\n" +
     ");"
@@ -49,15 +55,15 @@ connection.query(
     "CREATE UNIQUE INDEX UNIQUE_PUBLIC_KEY ON PUBLIC_KEY_MAINNET (public_key_string)"
 )
 
+connection.query(
+    "CREATE UNIQUE INDEX UNIQUE_ADDRESS_ID_FK_PUBLIC_KEY_ID ON ACCOUNT_IDS_MAINNET (address_id, FK_PUBLIC_KEY_ID, is_deleted);"
+)
+
 async function getPublicKeyId(signerId){
     let sql = "SELECT * FROM PUBLIC_KEY_MAINNET WHERE public_key_string = " + "'" + signerId + "'" + ";";
     const results = await connection.promise().query(sql);
-    console.log(" SIGNER ID ", signerId);
-    console.log(" RESULTS ", results[0]);
-
-    log.debug(" >>>>>>>>> RESULTS >>>>>>>>>>>>>>> ", results);
-    log.debug(" >>>>>>>>> RESULTS[0] >>>>>>>>>>>>>>> ", results[0]);
-
+    log.debug(" SIGNER ID ", signerId);
+    log.debug(" RESULTS ", results[0]);
 
     return results[0][0].id;
 }
@@ -65,34 +71,36 @@ async function getPublicKeyId(signerId){
 async function removeAllAddressIdsAfterJoiningPublicKey(public_key_string, addressId) {
     let sql = "DELETE FROM ACCOUNT_IDS_MAINNET WHERE FK_PUBLIC_KEY_ID = " + "(SELECT id FROM PUBLIC_KEY_MAINNET WHERE public_key_string = " + "'" + public_key_string + "'" + ")" + "AND address_id = " + "'" + addressId + "'" + ";";
     const results = await connection.promise().execute(sql);
+    log.debug("REMOVE RESULTS ", results);
     return results[0];
 }
 
-async function insertPublicKeySignerId(signerId) {
+async function softDeleteAllAddressIdsAfterJoiningPublicKey(public_key_string, addressId, deleteReceiptHash) {
+    let sql = "UPDATE ACCOUNT_IDS_MAINNET SET is_deleted = 1, delete_receipt_hash = " + "'" + deleteReceiptHash + "'" + " WHERE FK_PUBLIC_KEY_ID = " + "(SELECT id FROM PUBLIC_KEY_MAINNET WHERE public_key_string = " + "'" + public_key_string + "'" + ")" + "AND address_id = " + "'" + addressId + "'" + ";";
+    const results = await connection.promise().execute(sql);
+    log.debug("SOFT DELETE RESULTS", results);
+    return results[0];
+}
+
+async function insertPublicKeySignerId(signerId, blockHeight) {
     let sql = "INSERT INTO PUBLIC_KEY_MAINNET (public_key_string) VALUES "+ "('" + signerId + "')" + ";";
     const results = await connection.promise().execute(sql);
+    log.debug("INSERT PUBLIC KEY RESULTS", signerId, blockHeight);
     return results[0];
 }
 
-async function insertAddressIds(publicKeyId, addressId) {
-    let sql = "INSERT INTO ACCOUNT_IDS_MAINNET (FK_PUBLIC_KEY_ID, address_id) VALUES "+ "(" + publicKeyId + "," + "'" + addressId + "'" + ")";
+async function insertAddressIds(publicKeyId, addressId, blockHeight, receiptHash, saveType) {
+    const NOT_DELETED = 0;
+    let sql = "INSERT INTO ACCOUNT_IDS_MAINNET (FK_PUBLIC_KEY_ID, address_id, block_height, receipt_hash, save_type, is_deleted) VALUES " + "(" + publicKeyId + ", " + "'" + addressId + "', " + blockHeight + ", " + "'" + receiptHash + "', " + "'" + saveType + "', " + NOT_DELETED + ")" + ";";
     const results = await connection.promise().execute(sql);
-    return results[0];
-}
-
-
-async function insertNewAddressIds(addressId) {
-    const lastInsertIdBefore = await connection.promise().execute("SELECT LAST_INSERT_ID()");
-    const lastInsertId = lastInsertIdBefore[0][0]['LAST_INSERT_ID()'];
-    log.debug(" >>>>>>>>> LASTINSERTID >>>>>>>>>>>>>>> ", lastInsertId);
-    let sql = "INSERT INTO ACCOUNT_IDS_MAINNET (address_id, FK_PUBLIC_KEY_ID) VALUES "+ "('" + addressId + "', " + lastInsertId + ")";
-    const results = await connection.promise().execute(sql);
+    log.debug("INSERT ADDRESS IDS RESULTS", results);
     return results[0];
 }
 
 async function handleStreamerMessage(streamerMessage: types.StreamerMessage): Promise<void> {
     try {
-        log.info("Block Height >>>>>>>>>", streamerMessage.block.header.height);
+        const blockHeight = streamerMessage.block.header.height;
+        log.info("Block Height >>>>>>>>>", blockHeight);
 
         for (const shard of streamerMessage.shards) {
             log.info(" shardId: ", shard.shardId);
@@ -106,23 +114,59 @@ async function handleStreamerMessage(streamerMessage: types.StreamerMessage): Pr
                 if (actionList.actions !== []) {
 
                     for (const action of actionList.actions) {
-                        // if that is AddKey action
+                        log.debug(" receiptExecutionOutcome ACTION NOW >>>>>>>>>>>>>>>>>>>>>>>>>>>> ", action);
+
+                        // if create account
+                        if (action === 'CreateAccount') {
+                            log.debug(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> CREATE ACCOUNT >>>>>>>>>>>>>>>>>>>>>>>>>>>> ")
+                            for (const state of shard.stateChanges) {
+                                const receiptHash = receiptExecutionOutcome.receipt.receiptId;
+
+                                log.info(" STATE ", state, " BLOCK HEIGHT ", blockHeight, " RECEIPT HASH ", receiptHash);
+
+                                if (state['type'] === 'access_key_update') {
+                                    log.info("ACCESS_KEY_UPDATE");
+
+                                    const saveType = state['change']['accessKey']['permission']['FunctionCall'] ? 'FunctionCall' :
+                                        state['change']['accessKey']['permission'] === 'FullAccess' ? 'FullAccess' : 'ERROR';
+                                    const addressId = state['change']['accountId'];
+                                    const signerPublicKey = state['change']['publicKey'];
+
+                                    log.info(" ACCOUNT ID ", addressId, " PUBLIC KEY ", signerPublicKey, " BLOCK HEIGHT ", blockHeight);
+
+                                    try {
+                                        await insertPublicKeySignerId(signerPublicKey, blockHeight);
+                                    } catch (e) {
+                                        log.error("Duplicate Entry", streamerMessage.block.header.height, shard.shardId);
+                                    }
+                                    const publicKeyId = await getPublicKeyId(signerPublicKey);
+                                    await insertAddressIds(publicKeyId, addressId, blockHeight, receiptHash, saveType);
+                                }
+                            }
+                            break;
+                        }
+
+                        // if that is AddKey action, not creating new account
                         if (action.AddKey) {
-                            log.debug("ADD KEY >>>>>>>>>>>>>>>>>>>>", streamerMessage.block.header.height, shard.shardId)
+                            log.debug("receiptExecutionOutcome ADD KEY >>>>>>>>>>>>>>>>>>>>")
 
                             const addressId = receiptExecutionOutcome.receipt.receipt['Action'].signerId;
-                            const signerPublicKey = receiptExecutionOutcome.receipt.receipt['Action'].signerPublicKey;
+                            const signerPublicKey = action.AddKey.publicKey;
 
-                            log.info(" addressId: ", addressId, streamerMessage.block.header.height, shard.shardId);
-                            log.info(" signerPublicKey ", signerPublicKey, streamerMessage.block.header.height, shard.shardId);
+                            log.info(" receiptExecutionOutcome addressId: ", addressId, streamerMessage.block.header.height, shard.shardId);
+                            log.info(" receiptExecutionOutcome signerPublicKey ", signerPublicKey, streamerMessage.block.header.height, shard.shardId);
 
                             try {
-                                await insertPublicKeySignerId(signerPublicKey);
+                                await insertPublicKeySignerId(signerPublicKey, blockHeight);
                             } catch (e) {
                                 log.error("Duplicate Entry", streamerMessage.block.header.height, shard.shardId);
                             }
+
                             const publicKeyId = await getPublicKeyId(signerPublicKey);
-                            await insertAddressIds(publicKeyId, addressId);
+                            const receiptHash = receiptExecutionOutcome.receipt.receiptId;
+                            const saveType = action.AddKey.accessKey.permission.toString() === 'FullAccess' ? 'FullAccess' : 'FunctionCall';
+
+                            await insertAddressIds(publicKeyId, addressId, blockHeight, receiptHash, saveType);
                         }
 
                         // if that is DeleteKey action
@@ -130,19 +174,20 @@ async function handleStreamerMessage(streamerMessage: types.StreamerMessage): Pr
                             log.debug("DELETE KEY >>>>>>>>>>>>>>>>>>>>")
 
                             const addressId = receiptExecutionOutcome.receipt.receipt['Action'].signerId;
-                            const signerPublicKey = receiptExecutionOutcome.receipt.receipt['Action'].signerPublicKey;
+                            const signerPublicKey = action.DeleteKey.publicKey;
+                            const deleteReceiptHash = receiptExecutionOutcome.receipt.receiptId;
 
-                            log.info(" DELETE KEY signerId: ", addressId, streamerMessage.block.header.height, shard.shardId);
-                            log.info(" DELETE KEY signerPublicKey ", signerPublicKey, streamerMessage.block.header.height, shard.shardId);
+                            log.info(" signerId: ", addressId, streamerMessage.block.header.height, shard.shardId);
+                            log.info(" signerPublicKey ", signerPublicKey, streamerMessage.block.header.height, shard.shardId);
 
-                            await removeAllAddressIdsAfterJoiningPublicKey(signerPublicKey, addressId);
+                            await softDeleteAllAddressIdsAfterJoiningPublicKey(signerPublicKey, addressId, deleteReceiptHash);
                         }
                     }
                 }
             }
         }
     } catch (e) {
-        log.warn(e);
+        log.info(" SOME THING ERROR", e);
         log.info("FAILED Block Height >>>>>>>>>", streamerMessage.block.header.height);
     }
 }
